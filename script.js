32360ac4d8a28bfe15e051ba26384912fce8b980
jesse-robledo <robledo.josej@gmail.com>
2022-06-14 15:39:18 -0600

    removed postal code from city display

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const cardContainer = document.querySelector(".card-div");
 const locationText = document.querySelector(".location");
 const clearSearch = document.querySelector(".clear-search");
 const locSearch = document.querySelector("[data-search]");
+const body = document.querySelector("body");
 
-const findLocation = function (city, state = "") {
+const findLocation = function (city, state = "", country = "US") {
   fetch(
-    `https://api.geocod.io/v1.7/geocode?format=simple&city=${city}&state=${state}&api_key=${geoApi}`
+    `https://api.geocod.io/v1.7/geocode?format=simple&city=${city}&state=${state}&country=${country}&api_key=${geoApi}`
   )
     .then((response) => response.json())
     .then((data) => {
-      locationText.textContent = data.address;
+      locationString = data.address.substring(0, data.address.lastIndexOf(" "));
+      locationText.textContent = locationString;
       const { lat, lng: lon } = data;
       getWeatherData(lat, lon);
     });
@@ -31,6 +33,12 @@ const windDirection = function (degree) {
   degree = Math.round((degree * 8) / 360);
   return directions[degree];
 };
+
+const padString = function (value, numSpaces, char) {
+  return String(value).padStart(numSpaces, char);
+};
+
+const setBackground = function (hour) {};
 
 const weatherIconURL = {
   "thunderstorm with light rain": "wi-storm-showers",
@@ -98,6 +106,7 @@ const generateForecast = function (arr) {
   if (cardContainer.textContent.length > 0) {
     cardContainer.textContent = "";
   }
+
   arr.forEach((element) => {
     let {
       dt: date,
@@ -114,6 +123,10 @@ const generateForecast = function (arr) {
       weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
     sunrise = new Date(sunrise * 1000);
     sunset = new Date(sunset * 1000);
+    sunriseHours = sunrise.getHours();
+    sunriseMinutes = sunrise.getMinutes();
+    sunsetHours = sunset.getHours();
+    sunsetMinutes = sunset.getMinutes();
     date = new Date(date * 1000);
     month = date.getMonth() + 1;
     day = String(date.getDate()).padStart(2, "0");
@@ -129,32 +142,42 @@ const generateForecast = function (arr) {
     <h3 class='date'>${month + "/" + day}</h3>
     <div class="card flex-card">
 
-      <h3 class="high flex-cast">&nbsp;<sup><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M352 352c-8.188 0-16.38-3.125-22.62-9.375L192 205.3l-137.4 137.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25C368.4 348.9 360.2 352 352 352z"/></svg></sup>&nbsp;${Math.round(
-        max
+      <h3 class="high flex-cast"><sup><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M352 352c-8.188 0-16.38-3.125-22.62-9.375L192 205.3l-137.4 137.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25C368.4 348.9 360.2 352 352 352z"/></svg></sup>${padString(
+        Math.round(max),
+        4,
+        " "
       )}°</h3>
       <i class="wi ${weatherIcon} flex-cast"></i>
-      <h3 class="low flex-cast"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"/></svg>&nbsp;${Math.round(
-        min
+      <h3 class="low flex-cast"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"/></svg>${padString(
+        Math.round(min),
+        4,
+        " "
       )}°</h3>
       <h3 class="forecast-description">${weatherText}.</h3>
       
       <div class="info flex-info-row">
           <h3 class="flex-info-col"><i class="wi wi-sunrise"></i>&nbsp;${
-            String(sunrise.getHours()).padStart(2, "0") +
+            padString(sunriseHours, 2, "0") +
             ":" +
-            String(sunrise.getMinutes()).padStart(2, "0")
+            padString(sunriseMinutes, 2, "0")
           }</h3>
           <h3 class="flex-info-col"><i class="wi wi-sunset"></i>&nbsp;${
-            String(sunset.getHours()).padStart(2, "0") +
+            padString(sunsetHours, 2, "0") +
             ":" +
-            String(sunset.getMinutes()).padStart(2, "0")
+            padString(sunsetMinutes, 2, "0")
           }</h3>
       </div>
       <div class="info flex-info-row">
-      <h3 class="flex-info-col"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M416 128c-.625 0-1.125 .25-1.625 .25C415.5 123 416 117.6 416 112c0-44.25-35.75-80-79.1-80c-24.62 0-46.25 11.25-60.1 28.75C256.4 24.75 219.3 0 176 0C114.3 0 64 50.13 64 112c0 7.25 .7512 14.25 2.126 21.25C27.76 145.8 .0054 181.5 .0054 224c0 53 42.1 96 95.1 96h319.1C469 320 512 277 512 224S469 128 416 128zM198.8 353.9c-12.17-5.219-26.3 .4062-31.52 12.59l-47.1 112c-5.219 12.19 .4219 26.31 12.61 31.53C134.1 511.4 138.2 512 141.3 512c9.312 0 18.17-5.438 22.08-14.53l47.1-112C216.6 373.3 210.1 359.2 198.8 353.9zM81.46 353.9c-12.19-5.219-26.3 .4062-31.52 12.59l-47.1 112C-3.276 490.7 2.365 504.8 14.55 510.1C17.63 511.4 20.83 512 23.99 512c9.312 0 18.17-5.438 22.08-14.53l47.1-112C99.29 373.3 93.64 359.2 81.46 353.9zM316.1 353.9c-12.19-5.219-26.3 .4062-31.52 12.59l-47.1 112c-5.219 12.19 .4219 26.31 12.61 31.53C252.3 511.4 255.5 512 258.7 512c9.312 0 18.17-5.438 22.08-14.53l47.1-112C333.1 373.3 328.3 359.2 316.1 353.9zM433.5 353.9c-12.17-5.219-26.28 .4062-31.52 12.59l-47.1 112c-5.219 12.19 .4219 26.31 12.61 31.53C369.6 511.4 372.8 512 375.1 512c9.312 0 18.17-5.438 22.08-14.53l47.1-112C451.3 373.3 445.6 359.2 433.5 353.9z"/></svg></i>&nbsp;${Math.round(
-        pop * 100
+      <h3 class="flex-info-col"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M416 128c-.625 0-1.125 .25-1.625 .25C415.5 123 416 117.6 416 112c0-44.25-35.75-80-79.1-80c-24.62 0-46.25 11.25-60.1 28.75C256.4 24.75 219.3 0 176 0C114.3 0 64 50.13 64 112c0 7.25 .7512 14.25 2.126 21.25C27.76 145.8 .0054 181.5 .0054 224c0 53 42.1 96 95.1 96h319.1C469 320 512 277 512 224S469 128 416 128zM198.8 353.9c-12.17-5.219-26.3 .4062-31.52 12.59l-47.1 112c-5.219 12.19 .4219 26.31 12.61 31.53C134.1 511.4 138.2 512 141.3 512c9.312 0 18.17-5.438 22.08-14.53l47.1-112C216.6 373.3 210.1 359.2 198.8 353.9zM81.46 353.9c-12.19-5.219-26.3 .4062-31.52 12.59l-47.1 112C-3.276 490.7 2.365 504.8 14.55 510.1C17.63 511.4 20.83 512 23.99 512c9.312 0 18.17-5.438 22.08-14.53l47.1-112C99.29 373.3 93.64 359.2 81.46 353.9zM316.1 353.9c-12.19-5.219-26.3 .4062-31.52 12.59l-47.1 112c-5.219 12.19 .4219 26.31 12.61 31.53C252.3 511.4 255.5 512 258.7 512c9.312 0 18.17-5.438 22.08-14.53l47.1-112C333.1 373.3 328.3 359.2 316.1 353.9zM433.5 353.9c-12.17-5.219-26.28 .4062-31.52 12.59l-47.1 112c-5.219 12.19 .4219 26.31 12.61 31.53C369.6 511.4 372.8 512 375.1 512c9.312 0 18.17-5.438 22.08-14.53l47.1-112C451.3 373.3 445.6 359.2 433.5 353.9z"/></svg></i>${padString(
+        Math.round(pop * 100),
+        4,
+        " "
       )}%</h3>
-      <h3 class="flex-info-col"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M16 319.1C16 245.9 118.3 89.43 166.9 19.3C179.2 1.585 204.8 1.585 217.1 19.3C265.7 89.43 368 245.9 368 319.1C368 417.2 289.2 496 192 496C94.8 496 16 417.2 16 319.1zM112 319.1C112 311.2 104.8 303.1 96 303.1C87.16 303.1 80 311.2 80 319.1C80 381.9 130.1 432 192 432C200.8 432 208 424.8 208 416C208 407.2 200.8 400 192 400C147.8 400 112 364.2 112 319.1z"/></svg></i>&nbsp;${humidity}%</h3>
+      <h3 class="flex-info-col"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M16 319.1C16 245.9 118.3 89.43 166.9 19.3C179.2 1.585 204.8 1.585 217.1 19.3C265.7 89.43 368 245.9 368 319.1C368 417.2 289.2 496 192 496C94.8 496 16 417.2 16 319.1zM112 319.1C112 311.2 104.8 303.1 96 303.1C87.16 303.1 80 311.2 80 319.1C80 381.9 130.1 432 192 432C200.8 432 208 424.8 208 416C208 407.2 200.8 400 192 400C147.8 400 112 364.2 112 319.1z"/></svg></i>${padString(
+        humidity,
+        4,
+        " "
+      )}%</h3>
   </div>
 
     </div>
@@ -170,8 +193,10 @@ const getWeatherData = function (lat, lon) {
   )
     .then((response) => response.json())
     .then((data) => {
+      localTime = new Date(data.current.dt * 1000);
+      localTime = localTime.getHours();
       forecastArr = data.daily.slice(0, 5);
-      console.log(forecastArr);
+      //setBackground(localTime);
       generateForecast(forecastArr);
     });
 };
@@ -179,13 +204,7 @@ const getWeatherData = function (lat, lon) {
 locSearch.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     e.preventDefault();
-    const [city, state] = e.target.value.match(/\b(\w+)\b/g);
-    console.log(city, state);
-    findLocation(city, state);
+    const [city, state, country] = e.target.value.match(/\b(\w+)\b/g);
+    findLocation(city, state, country);
   }
-});
-
-clearSearch.addEventListener("click", () => {
-  locSearch.value = "";
-  clearSearch.style.visibility = "hidden";
 });
