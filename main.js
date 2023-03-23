3b3dc45f556c2dd7f620247125c1046ca74b8ba9
Jesse Robledo <robledo.josej@gmail.com>
2023-03-22 22:20:43 -0600

    renamed index.js to main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
+import { findLocation } from './geolocation.js';
 const locationText = document.querySelector(".location");
 const locSearch = document.querySelector("[data-search]");
 
-import { findLocation } from './geolocation.js';
-
-
-locSearch.addEventListener("keypress", function (e) {
+locSearch.addEventListener("keypress", async function (e) {
   if (e.key === "Enter") {
     e.preventDefault();
     const [city, state, country] = e.target.value.match(/\b(\w+)\b/g);
-    findLocation(city, state, country);
+    await findLocation(city, state, country);
   }
 });
 
