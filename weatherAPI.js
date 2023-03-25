42d83212376de392eb27fe051b96b21da6c19baa
Jesse Robledo <robledo.josej@gmail.com>
2023-03-24 19:40:11 -0600

    moved openweathermap api calls to their own module

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,18 +1,37 @@
-import {locationText} from "./main.js";
+import {locationText} from "./index.js";
 import { generateForecast } from './weatherHTML.js'
 
-const weatherApi = process.env.WEATHER_API_KEY
+// const getWeatherData = async function (lat, lon) {
+//     try {
+//         const response = await fetch(`./.netlify/functions/token-hider/getLocations?lat=${lat}?lon=${lon}`, {
+//             method: 'GET',
+//             headers: {
+//                 'Accept': 'application/json',
+//             }})
+//         const data = await response.json();
+//         const forecastData = data.daily.slice(0, 5);
+//         const alertData = data.alerts;
+//         generateForecast(forecastData, alertData);
+//     }
+//     catch (err) {
+//         locationText.textContent = "Error generating forecast.";
+//     }
+// };
+
+
 const getWeatherData = async function (lat, lon) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely&appid=${weatherApi}`)
+        const queryString = new URLSearchParams({ api: 'weather', lat, lon }).toString();
+        const response = await fetch(`/.netlify/functions/api-handler?${queryString}`);
         const data = await response.json();
+
         const forecastData = data.daily.slice(0, 5);
         const alertData = data.alerts;
         generateForecast(forecastData, alertData);
-    }
-    catch (err) {
+    } catch (err) {
         locationText.textContent = "Error generating forecast.";
     }
 };
+
 
 export { getWeatherData };
